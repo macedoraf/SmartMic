42bb610a47d897a22e1c76305042c054e34f98a9
Rafael Macedo <rafael.macedo@resourceit.com>
2019-09-23 00:43:02 -0300

    added mainActivity

---
+++ app/src/main/java/rafael/macedo/project/presentation/FragmentHomeViewPager.kt
@@ -0,0 +1,18 @@
+package rafael.macedo.project.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentHomeViewPager:Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+//        val binding = FragmentHomeViewPager.inflate(inflater, container, false)
+//        val tabLayout = binding.tabs
+//        val viewPager = binding.viewPager
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/rafael/macedo/project/presentation/MainActivity.kt
@@ -0,0 +1,13 @@
+package rafael.macedo.project.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import rafael.macedo.project.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
